7f5b3f44a86f1e6afc056931f38382b397b6f674
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-02 14:47:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let saveBtn = document.querySelector("#btn");
+let userFontSize = document.querySelector("#fontsize");
+let userFontColor = document.querySelector("#fontcolor");
+let form = document.querySelector("form");
+
+saveBtn.addEventListener("click", () => {
+	document.cookie = `fontsize=${userFontSize.value}px`;
+	document.cookie = `fontcolor=${userFontColor.value}`;
+	form.style.fontSize = `${userFontSize.value}px`
+	form.style.color = `${userFontSize.value}`
+});
