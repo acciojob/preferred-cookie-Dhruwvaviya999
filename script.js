e9328149bed8461352c84198b0985ac77912d70e
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-02 15:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-let saveBtn = document.querySelector("#btn");
-let userFontSize = document.querySelector("#fontsize");
-let userFontColor = document.querySelector("#fontcolor");
-let form = document.querySelector("form");
+function getCookie(name) {
+	let cookies = document.cookie.split('; ');
+	for (let cookie of cookies) {
+		let [key, value] = cookie.split('=');
+		if (key === name) return value;
+	}
+	return null;
+}
 
-saveBtn.addEventListener("click", () => {
-	document.cookie = `fontsize=${userFontSize.value}px`;
-	document.cookie = `fontcolor=${userFontColor.value}`;
-	form.style.fontSize = `${userFontSize.value}px`
-	form.style.color = `${userFontSize.value}`
-});
+// Function to set a cookie
+function setCookie(name, value, days) {
+	let expires = "";
+	if (days) {
+		let date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + value + "; path=/" + expires;
+}
+
+window.onload = function () {
+	let savedFontSize = getCookie("fontsize");
+	let savedFontColor = getCookie("fontcolor");
+
+	if (savedFontSize) {
+		document.documentElement.style.setProperty('--fontsize', savedFontSize + 'px');
+		document.getElementById("fontsize").value = savedFontSize;
+	}
+	if (savedFontColor) {
+		document.documentElement.style.setProperty('--fontcolor', savedFontColor);
+		document.getElementById("fontcolor").value = savedFontColor;
+	}
+};
+
+document.querySelector("#btn").addEventListener("click", () => {
+	event.preventDefault();
+	let fontSize = document.getElementById("fontsize").value;
+	let fontColor = document.getElementById("fontcolor").value;
+
+	setCookie("fontsize", fontSize, 365);
+	setCookie("fontcolor", fontColor, 365);
+
+	document.documentElement.style.setProperty('--fontsize', fontSize + 'px');
+	document.documentElement.style.setProperty('--fontcolor', fontColor);
+})
